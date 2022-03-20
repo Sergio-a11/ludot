ce55245b024ca4aad14bfdcb238631bfcd609a89
Sergio-a11 <cz.andrs@gmail.com>
2022-03-20 18:19:39 -0500

    first

---
+++ src/test/java/com/ludo/ludoteca/LudotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ludo.ludoteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LudotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
